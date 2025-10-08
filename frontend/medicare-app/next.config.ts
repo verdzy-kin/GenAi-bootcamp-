5300b89b29fcc82cf7dce9363a3f7f1295259be4
Delsy-Kinyuy <kinyuydelsy6@gmail.com>
2025-10-08 05:30:42 +0100

    Add Medicare AI frontend application

---
+++ frontend/medicare-app/next.config.ts
@@ -0,0 +1,23 @@
+import type { NextConfig } from "next";
+
+const nextConfig: NextConfig = {
+  /* config options here */
+};
+
+export default nextConfig;
+
+
+// import type { Config } from "tailwindcss";
+
+// const config: Config = {
+//   content: [
+//     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./components/**/*.{js,ts,jsx,tsx,mdx}",
+//     "./app/**/*.{js,ts,jsx,tsx,mdx}",
+//   ],
+//   theme: {
+//     extend: {},
+//   },
+//   plugins: [],
+// };
+// export default config;
